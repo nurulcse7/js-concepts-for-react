efc6ecb3e615fa2ddc3a1df31556b37677307e7b
nurulcse7 <nurul.cse7@gmail.com>
2022-09-12 17:44:32 +0600

    Updated fetch.js

--- fetch.js
+++ fetch.js
@@ -36,7 +36,7 @@ const products = [
     { name: 'laptop', price: 3200, brand: 'len', color: 'silver' },
     { name: 'phone', price: 7000, brand: 'HTC', color: 'golden' },
     { name: 'watch', price: 3000, brand: 'casio', color: 'yellow' },
-    { name: 'sungalss', price: 300, brand: 'ray', color: 'black' },
+    { name: 'sunglass', price: 300, brand: 'ray', color: 'black' },
     { name: 'camera', price: 9000, brand: 'canon', color: 'gray' }
 ];
 
@@ -47,3 +47,5 @@ const newProducts = [...products, newProduct];
 // create a new array without one specific item 
 // remove phone means create a new array without the phone
 const remaining = products.filter(p => p.name !== 'phone');
+
+// p = product
